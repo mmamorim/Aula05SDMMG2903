f5332271a9ae7576fab5c3c728907b9be382ed54
mmamorim <marc.m.amorim@gmail.com>
2022-03-29 21:54:12 -0300

    versão final

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 
 
-console.log("oi gente");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Oi gente....alguém fez uma requisição GET na rota /');
+})
+
+app.get('/teste', (req, res) => {
+  res.send('<h3>Oi gente....alguém fez uma requisição GET na rota /teste</h3>');
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
